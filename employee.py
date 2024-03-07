2b3ebfd207b3bb7ea5b3b10c6d071f198e1b3bcb
LeBarn <105302036+LeBarn@users.noreply.github.com>
2024-03-07 22:34:15 +0000

    answer2 employee.py

--- employee.py
+++ employee.py
@@ -29,4 +29,10 @@ class Employee:
             role (str): The role of the employee.
             salary (float): The salary of the employee.
         """
+
+        self.name = name
+        self.emp_id= emp_id
+        self.role = role
+        self.salary = salary
+
         pass
