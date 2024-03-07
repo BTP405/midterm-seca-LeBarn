80c723e362407eea0bb8f3a17cf73ea97eee8513
LeBarn <105302036+LeBarn@users.noreply.github.com>
2024-03-07 23:11:04 +0000

    answers1 task

--- task.py
+++ task.py
@@ -15,6 +15,12 @@ class Task:
         status (str): The status of the task.
         project (Project): The project associated with the task.
     """
+    str t_task_id
+    str t_description
+    str t_deadline
+    str t_status
+    str t_status
+    Project t_project
 
     def __init__(self, task_id, description, deadline, status, project):
         """
@@ -27,4 +33,10 @@ class Task:
             status (str): The status of the task.
             project (Project): The project associated with the task.
         """
+        self.t_task_id = task_id
+        self.t_description = description
+        self.t_deadline = deadline
+        self.t_status = status
+        self.t_project = project
+
         pass
